41c50a676a7945e79b984fab8fa37114f7f518d5
adreghiciu <adreghiciu@gmail.com>
2008-01-10 11:45:28 +0000

    Add JavaDoc and review implementation.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/swissbox@10073 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- src/main/java/org/ops4j/pax/swissbox/lang/BundleClassLoader.java
+++ src/main/java/org/ops4j/pax/swissbox/lang/BundleClassLoader.java
@@ -19,29 +19,22 @@ package org.ops4j.pax.swissbox.lang;
 
 import java.io.IOException;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.Enumeration;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.Bundle;
 import org.ops4j.lang.PreConditionException;
 
 /**
- * Class loader that uses the a bundle in order to implement classloader functionality.
+ * Class loader that uses the a bundle in order to implement class loader functionality.
  *
  * @author Alin Dreghiciu
  * @since 0.1.0, December 29, 2007
  */
 public class BundleClassLoader
-    extends URLClassLoader
+    extends ClassLoader
 {
 
-    /**
-     * Logger.
-     */
-    private static final Log LOG = LogFactory.getLog( BundleClassLoader.class );
     /**
      * Bundle used for class loading.
      */
@@ -51,19 +44,20 @@ public class BundleClassLoader
      * Privileged factory method.
      *
      * @param bundle bundle to be used for class loading. Cannot be null.
+     * @param parent parent clas loader
      *
      * @return created bundle class loader
      *
-     * @see org.ops4j.pax.swissbox.lang.BundleClassLoader#BundleClassLoader(Bundle)
+     * @see org.ops4j.pax.swissbox.lang.BundleClassLoader#BundleClassLoader(Bundle,ClassLoader)
      */
-    public static BundleClassLoader newPriviledged( final Bundle bundle )
+    public static BundleClassLoader newPriviledged( final Bundle bundle, final ClassLoader parent )
     {
         return AccessController.doPrivileged(
             new PrivilegedAction<BundleClassLoader>()
             {
                 public BundleClassLoader run()
                 {
-                    return new BundleClassLoader( bundle );
+                    return new BundleClassLoader( bundle, parent );
                 }
             }
         );
@@ -73,12 +67,13 @@ public class BundleClassLoader
      * Creates a bundle class loader.
      *
      * @param bundle bundle to be used for class loading. Cannot be null.
+     * @param parent parent class loader
      */
-    public BundleClassLoader( final Bundle bundle )
+    public BundleClassLoader( final Bundle bundle, final ClassLoader parent )
     {
-        super( new URL[]{ } );
+        super( parent );
         PreConditionException.validateNotNull( bundle, "Bundle" );
-        this.m_bundle = bundle;
+        m_bundle = bundle;
     }
 
     /**
@@ -86,17 +81,52 @@ public class BundleClassLoader
      *
      * @return the bundle the class loader loads from
      */
-    protected Bundle getBundle()
+    public Bundle getBundle()
     {
         return m_bundle;
     }
 
+    /**
+     * If there is a parent class loader use the super implementation that will first use the parent and as a fallback
+     * it will call findResource(). In case there is no parent directy use findResource() as if we call the super
+     * implementation it will use the VMClassLoader, fact that should be avoided.
+     *
+     * @see ClassLoader#getResource(String)
+     */
     @Override
     public URL getResource( final String name )
     {
+        if( getParent() != null )
+        {
+            return super.getResource( name );
+        }
         return findResource( name );
     }
 
+    /**
+     * If there is a parent class loader use the super implementation that will first use the parent and as a fallback
+     * it will call findResources(). In case there is no parent directy use findResources() as if we call the super
+     * implementation it will use the VMClassLoader, fact that should be avoided.
+     *
+     * @see ClassLoader#getResources(String)
+     */
+    @Override
+    @SuppressWarnings( "unchecked" )
+    public Enumeration<URL> getResources( final String name )
+        throws IOException
+    {
+        if( getParent() != null )
+        {
+            super.getResource( name );
+        }
+        return findResources( name );
+    }
+
+    /**
+     * Use bundle to find find the class.
+     *
+     * @see ClassLoader#findClass(String)
+     */
     @Override
     protected Class findClass( final String name )
         throws ClassNotFoundException
@@ -104,11 +134,22 @@ public class BundleClassLoader
         return m_bundle.loadClass( name );
     }
 
+    /**
+     * If there is a parent class loader use the super implementation that will first use the parent and as a fallback
+     * it will call findClass(). In case there is no parent directy use findClass() as if we call the super
+     * implementation it will use the VMClassLoader, fact that should be avoided.
+     *
+     * @see ClassLoader#getResource(String)
+     */
     @Override
     protected Class loadClass( final String name, final boolean resolve )
         throws ClassNotFoundException
     {
-        Class classToLoad = findClass( name );
+        if( getParent() != null )
+        {
+            return super.loadClass( name, resolve );
+        }
+        final Class classToLoad = findClass( name );
         if( resolve )
         {
             resolveClass( classToLoad );
@@ -116,15 +157,25 @@ public class BundleClassLoader
         return classToLoad;
     }
 
+    /**
+     * Use bundle to find resource.
+     *
+     * @see ClassLoader#findResource(String)
+     */
     @Override
-    public URL findResource( final String name )
+    protected URL findResource( final String name )
     {
         return m_bundle.getResource( name );
     }
 
+    /**
+     * Use bundle to find resources.
+     *
+     * @see ClassLoader#findResources(String)
+     */
     @Override
     @SuppressWarnings( "unchecked" )
-    public Enumeration<URL> findResources( final String name )
+    protected Enumeration<URL> findResources( final String name )
         throws IOException
     {
         return m_bundle.getResources( name );
@@ -137,6 +188,7 @@ public class BundleClassLoader
             .append( this.getClass().getSimpleName() )
             .append( "{" )
             .append( "bundle=" ).append( m_bundle )
+            .append( ",parent=" ).append( getParent() )
             .append( "}" )
             .toString();
     }
@@ -160,14 +212,19 @@ public class BundleClassLoader
             return false;
         }
 
+        if( getParent() != null ? !getParent().equals( that.getParent() ) : that.getParent() != null )
+        {
+            return false;
+        }
+
         return true;
     }
 
     @Override
     public int hashCode()
     {
-        return ( m_bundle != null ? m_bundle.hashCode() : 0 );
+        return ( m_bundle != null ? m_bundle.hashCode() : 0 ) * 37
+               + ( getParent() != null ? getParent().hashCode() : 0 );
     }
-
 }
 
