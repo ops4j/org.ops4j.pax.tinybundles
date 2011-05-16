1bd6861daa13a1b0dc7e24980db14dc67f630585
Toni Menzel <toni@okidokiteam.com>
2011-05-16 22:28:09 +0200

    Fix packaging

--- pax-tinybundles-core/src/main/java/org/ops4j/pax/tinybundles/core/builders/SynchronousRawBuilder.java
+++ pax-tinybundles-core/src/main/java/org/ops4j/pax/tinybundles/core/builders/SynchronousRawBuilder.java
@@ -1,1 +1,56 @@
-/* * Copyright 2011 Toni Menzel. * * Licensed under the Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the License. * You may obtain a copy of the License at * * http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or * implied. * * See the License for the specific language governing permissions and * limitations under the License. */package org.ops4j.pax.tinybundles.core.builders;import java.io.File;import java.io.FileInputStream;import java.io.FileOutputStream;import java.io.IOException;import java.io.InputStream;import java.net.URL;import java.util.Map;import java.util.Set;import java.util.jar.JarEntry;import java.util.jar.JarOutputStream;import java.util.jar.Manifest;import org.slf4j.Logger;import org.slf4j.LoggerFactory;import org.ops4j.io.StreamUtils;import org.ops4j.pax.tinybundles.core.BuildableBundle;import org.ops4j.pax.tinybundles.core.intern.Info;/** * Alternative, early flushing version. */public class SynchronousRawBuilder extends RawBuilder {    private static Logger LOG = LoggerFactory.getLogger( SynchronousRawBuilder.class );     public InputStream build( final Map<String, URL> resources,                             final Map<String, String> headers )    {        LOG.debug( "make()" );        try {            File f = File.createTempFile( "temp", "bin" );            JarOutputStream jarOut = new JarOutputStream( new FileOutputStream( f ) );            try {                build( resources, headers, jarOut );            } finally {                jarOut.close();            }            return new FileInputStream( f );        } catch( IOException e ) {            throw new RuntimeException( e );        }    }}
+/*
+ * Copyright 2011 Toni Menzel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.tinybundles.core.builders;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Map;
+import java.util.jar.JarOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Alternative, early flushing version.
+ */
+public class SynchronousRawBuilder extends RawBuilder {
+
+    private static Logger LOG = LoggerFactory.getLogger( SynchronousRawBuilder.class );
+ 
+    public InputStream build( final Map<String, URL> resources,
+                             final Map<String, String> headers )
+    {
+        LOG.debug( "make()" );
+        try {
+            File f = File.createTempFile( "temp", "bin" );
+            JarOutputStream jarOut = new JarOutputStream( new FileOutputStream( f ) );
+            try {
+                build( resources, headers, jarOut );
+            } finally {
+                jarOut.close();
+            }
+            return new FileInputStream( f );
+
+        } catch( IOException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+}
