daf54ff5aebca18c855083cd9d24bbb54b929028
Toni Menzel <toni@okidokiteam.com>
2011-06-07 09:56:07 +0200

    Added initial package-info.java for public api package

---
+++ pax-tinybundles-core/src/main/java/org/ops4j/pax/tinybundles/core/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides the classes necessary to create and modify OSGi bundles from java api.
+ * <p>
+ * The {@link org.ops4j.pax.tinybundles.core.TinyBundles} class is the factory to get everything started.
+ *
+ * @since 1.0
+ */
+package org.ops4j.pax.tinybundles.core;
