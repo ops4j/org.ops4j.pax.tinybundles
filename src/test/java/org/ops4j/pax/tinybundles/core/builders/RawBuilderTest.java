27228769dab49327551e52a7a745d7947e3b6e1b
Toni Menzel <toni@okidokiteam.com>
2011-07-12 19:56:29 +0200

    added test

---
+++ src/test/java/org/ops4j/pax/tinybundles/core/builders/RawBuilderTest.java
@@ -0,0 +1,14 @@
+package org.ops4j.pax.tinybundles.core.builders;
+
+import org.junit.Test;
+import org.ops4j.pax.tinybundles.core.intern.RawBuilder;
+import org.ops4j.pax.tinybundles.core.intern.SynchronousRawBuilder;
+
+public class RawBuilderTest
+{
+    @Test
+    public void foo() {
+       RawBuilder builder = new SynchronousRawBuilder();
+       
+    }
+}
